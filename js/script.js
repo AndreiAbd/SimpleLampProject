e7a934529fcfae9ea585c245f5c64cb6e7f3260f
CoderX <73896715+AndreiAbd@users.noreply.github.com>
2022-02-12 07:08:46 +0200

    CoderX

--- js/script.js
+++ js/script.js
@@ -15,40 +15,47 @@ Github    :  https://github.com/AndreiAbd
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 document.addEventListener("keydown", (e) => {
-  // USE THIS TO DISABLE CONTROL AND ALL FUNCTION KEYS
-  // if (e.ctrlKey || (e.keyCode>=112 && e.keyCode<=123)) {
-  // THIS WILL ONLY DISABLE CONTROL AND F12
-  if (e.ctrlKey || e.keyCode==123) {
-    e.stopPropagation();
-    e.preventDefault();
-  }
+    // USE THIS TO DISABLE CONTROL AND ALL FUNCTION KEYS
+    // if (e.ctrlKey || (e.keyCode>=112 && e.keyCode<=123)) {
+    // THIS WILL ONLY DISABLE CONTROL AND F12
+    if (e.ctrlKey || e.keyCode == 123) {
+        e.stopPropagation();
+        e.preventDefault();
+    }
 });
 
 //Control Panel
-function lightOff(){
+function lightOff() {
     document.getElementById('l0').style.display = 'block';
     document.getElementById('l1').style.display = 'none';
 }
-function lightOn(){
+
+function lightOn() {
     document.getElementById('l0').style.display = 'none';
     document.getElementById('l1').style.display = 'block';
-// Timer part
-var timeleft = 10;
-var downloadTimer = setInterval(function(){
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementById("countdown").innerHTML = "Finished";
-    lightOff();
-  }else if(document.getElementById('l1').style.display === 'none'){
-    clearInterval(downloadTimer);
-    document.getElementById("countdown").innerHTML = "Finished";
-    lightOff();
-  } else {
-    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-  }
-  timeleft -= 1;
-}, 1000);
+    // Timer part
+    var timeleft = 10;
+    var downloadTimer = setInterval(function() {
+        if (timeleft <= 0) {
+            clearInterval(downloadTimer);
+            document.getElementById("countdown").innerHTML = "Finished";
+            lightOff();
+        } else if (document.getElementById('l1').style.display === 'none') {
+            clearInterval(downloadTimer);
+            document.getElementById("countdown").innerHTML = "Finished";
+            lightOff();
+        } else {
+            document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+        }
+        timeleft -= 1;
+    }, 1000);
 }
 //Get Current Year
 const d = new Date();
 document.getElementById("getCurrentYear").innerHTML = d.getFullYear()
+
+//Loader Page
+window.addEventListener("load", function() {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden";
+});
